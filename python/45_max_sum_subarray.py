bafe850bb8426a676baad818a5b04baba58080c4
mac mini wqs <wqs>
2022-11-14 16:04:10 +0800

    add

--- python/45_max_sum_subarray.py
+++ python/45_max_sum_subarray.py
@@ -7,13 +7,23 @@ The contiguous array [4, –1, 2, 1] has the largest sum = 6.
 """
 
 
+from curses.ascii import SO
+
+
 class Solution:
     def maxSubArray(self, nums: 'List[int]') -> 'int':
+    # def maxSubArray(self, nums: 'List[int]') -> 'int':
         """
         O(n) runtime, O(1) space – Dynamic programming
         """
         max_ending_here, max_so_far = nums[0], nums[0]
         for i in range(1, len(nums)):
+            # wqs greedy method, or the dp with memorize
             max_ending_here = max(max_ending_here + nums[i], nums[i])
             max_so_far = max(max_ending_here, max_so_far)
         return max_so_far
+
+test=[2, 1, -3, 4, -1, 2, 1, -5, 4]
+res = Solution().maxSubArray(test)
+# print(f"the res is")
+print(f"{res = }")

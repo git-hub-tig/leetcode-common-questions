c8865978f77d483d83bcb827110cd5375b8debc2
mac mini wqs <wqs>
2024-01-05 00:27:40 +0800

    unordered_map

--- cpp/03_two_sum_III.cpp
+++ cpp/03_two_sum_III.cpp
@@ -9,6 +9,8 @@
 //add(1); add(3); add(5); find(4) - true; find(7) - false
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+
 using namespace std;
 
 class TwoSum {
@@ -16,9 +18,9 @@ private:
     vector<int> data;
 public:
     void add(int number) {
-        data.insert(number);
+        data.push_back(number);
     }
-    bool find(int value) {
+    bool find(int target) {
         unordered_map<int, int> hash;
         for (int i = 0; i < data.size(); i++) {
             int numberToFind = target - data[i];
@@ -32,3 +34,22 @@ public:
         return false;
     }
 };
+/*
+no instance of overloaded function "std::__1::vector<_Tp, _Allocator>::insert [with _Tp=int, _Allocator=std::__1::allocator<int>]" matches the argument listC/C++(304)
+03_two_sum_III.cpp(19, 14): argument types are: (int)
+03_two_sum_III.cpp(19, 14): object type is: std::__1::vector<int, std::__1::allocator<int>>
+
+*/
+
+int main(){
+    auto s = TwoSum ();
+    s.add (1);
+    s.add (3);
+    s.add (5);
+
+    cout << s.find (4) << endl;
+    //  - true;
+
+    cout << s.find (7) << endl;
+    // - false
+}
